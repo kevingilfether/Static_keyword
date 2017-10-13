ea3d04888ff09a6a2eb34162fbe0ac322076a694
kevingilfether <kevingilfether@gmail.com>
2017-10-13 09:49:40 -0400

    Initial commit

---
+++ Static_keyword/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_keyword
+{
+    class Cat
+    {
+        private int catCount;
+
+        public int CatCount
+        {
+            get { return this.catCount; }
+            set { this.catCount = value; }
+        }
+
+        public Cat()
+        {
+
+        }
+
+        public void WarCry()
+        {
+            Console.WriteLine("We are an army of " + catCount + " cats!!");
+        }
+
+    }
+}
---
+++ Static_keyword/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
