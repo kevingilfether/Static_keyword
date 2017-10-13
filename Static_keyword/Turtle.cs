840e04e720790ecb91e5514cf4fc6a36038b9ecc
kevingilfether <kevingilfether@gmail.com>
2017-10-13 09:53:14 -0400

    Made turtle class

---
+++ Static_keyword/Turtle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_keyword
+{
+    class Turtle
+    {
+        static int turtleCount;
+
+        public Turtle()
+        {
+            turtleCount++;
+        }
+
+        public static void WarCry()
+        {
+            Console.WriteLine("We are an army of " + turtleCount + " turtles!!");
+        }
+
+    }
+}
