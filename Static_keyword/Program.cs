4a33118095dfbdf70d26578745695ad07e25c0ab
kevingilfether <kevingilfether@gmail.com>
2017-10-13 10:11:26 -0400

    Added battle code

--- Static_keyword/Program.cs
+++ Static_keyword/Program.cs
@@ -14,6 +14,36 @@ namespace Static_keyword
             List<Turtle> turtleArmy = new List<Turtle>();
 
             Random rand = new Random();
+
+            //making the armies
+            for (int i = 0; i < 100; i++)
+            {
+                if (rand.NextDouble() >= 0.5)
+                {
+                    catArmy.Add(new Cat());
+                }
+                else
+                {
+                    turtleArmy.Add(new Turtle());
+                }
+            }
+
+            //Code to tell each cat how many cats there are
+            //We'll need to run all this code any time we add a cat
+
+            int totalCats = catArmy.Count;
+
+            foreach (Cat cat in catArmy)
+            {
+                cat.CatCount = totalCats;
+            }
+
+            catArmy[0].WarCry();
+
+            //Code to tell each turtle how many
+            //turtles there are.
+
+            Turtle.WarCry();
         }
     }
 }
